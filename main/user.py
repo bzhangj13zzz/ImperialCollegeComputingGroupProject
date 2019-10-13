df0684c1f50cd8922b167466c024dda075daa1b3
halid1020 <haliteabudureyimu@gmail.com>
2019-10-13 21:35:59 +0100

    Deleted unuseful files

--- main/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self):
-        self.group_id = -1
-
-    def set_group_id(self, group_id):
-        self.group_id = group_id
-
-    def get_group_id(self):
-        return self.group_id
