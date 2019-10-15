045f2611b514eb0da1efc07553b316a88079d272
张博文 <zhangbowen@pc-139-139.customer.ask4.lan>
2019-10-15 19:53:08 +0100

    Fixed the basic functionalities

--- StudentGroupingOrganiser/src/main/java/ic/doc/sgo/springframework/Controller/GroupingController.java
+++ StudentGroupingOrganiser/src/main/java/ic/doc/sgo/springframework/Controller/GroupingController.java
@@ -2,24 +2,16 @@ package ic.doc.sgo.springframework.Controller;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 import ic.doc.sgo.Constraint;
 import ic.doc.sgo.Group;
 import ic.doc.sgo.Student;
-import ic.doc.sgo.groupingstrategies.RandomGroupingStrategy;
 import ic.doc.sgo.springframework.Service.GroupingService;
 import ic.doc.sgo.studentadapters.JsonStudentAdapter;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import javax.annotation.Resource;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class GroupingController {
-  @Resource GroupingService groupingService;
+  @Resource private GroupingService groupingService;
 
   @PostMapping(value = "/allocateGroups")
   public @ResponseBody String groupStudents(@RequestBody String json) {
@@ -38,7 +30,9 @@ public class GroupingController {
     JsonObject parsedJson = gson.fromJson(json, JsonObject.class);
     JsonArray studentsJsonArray = parsedJson.getAsJsonArray("students");
     JsonObject constraintJsonObject = parsedJson.getAsJsonObject("filters");
+    JsonObject groupedStudentsJsonObject = new JsonObject();
 
+    // Use Gson to convert json to object
     Constraint constraint = gson.fromJson(constraintJsonObject, Constraint.class);
 
     List<Student> studentList = new ArrayList<>();
@@ -48,30 +42,24 @@ public class GroupingController {
       optionalStudent.ifPresent(studentList::add);
     }
 
-    for (Student student : studentList) {
-      System.out.println(student.getId());
-    }
-
-    List<Group> groupList = new RandomGroupingStrategy().apply(studentList, constraint);
-    return groupListToJsonArray(groupList);
+    List<Group> groupList = groupingService.groupStudent(studentList, constraint);
+    groupedStudentsJsonObject.add("students", groupListToJsonArray(groupList));
+    // return a string which is in the form of a JsonObject
+    return gson.toJson(groupedStudentsJsonObject);
   }
 
-  private String groupListToJsonArray(List<Group> groupList) {
-    JsonArray array = new JsonArray();
-    List<String> list =
-        groupList
-            .stream()
-            .map(
-                group ->
-                    group
-                        .getStudents()
-                        .stream()
-                        .map(Student::toString)
-                        .collect(Collectors.toList()))
-            .collect(Collectors.toList())
-            .stream()
-            .flatMap(List::stream)
-            .collect(Collectors.toList());
-    return new Gson().toJson(array);
+  private JsonArray groupListToJsonArray(List<Group> groupList) {
+    JsonArray studentsJsonArray = new JsonArray();
+    for (Group group : groupList) {
+      for (Student student : group.getStudents()) {
+        JsonObject studentJsonObject = new JsonObject();
+        studentJsonObject.addProperty("id", student.getId());
+        // Add group id for each student json object
+        studentJsonObject.addProperty("groupId", String.valueOf(group.getId()));
+        studentJsonObject.addProperty("name", student.getName());
+        studentsJsonArray.add(studentJsonObject);
+      }
+    }
+    return studentsJsonArray;
   }
 }
