5403d35b63dc5840ba2589d822b0a9d0b5760982
Leonard Ge <gw980531@gmail.com>
2019-10-30 10:51:12 +0000

    Used the fixed point strategy so we take timezone into consideration.

--- src/main/java/ic/doc/sgo/springframework/Service/GroupingService.java
+++ src/main/java/ic/doc/sgo/springframework/Service/GroupingService.java
@@ -3,13 +3,13 @@ package ic.doc.sgo.springframework.Service;
 import ic.doc.sgo.Constraint;
 import ic.doc.sgo.Group;
 import ic.doc.sgo.Student;
-import ic.doc.sgo.groupingstrategies.RandomGroupingStrategy;
+import ic.doc.sgo.groupingstrategies.FixedPointStrategy;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroupingService {
   public List<Group> groupStudent(List<Student> studentList, Constraint constraint) {
-    return new RandomGroupingStrategy().apply(studentList,constraint);
+    return new FixedPointStrategy().apply(studentList,constraint);
   }
 }
